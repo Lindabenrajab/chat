4d73b2f547bdea381801b4af798a2b7b16763d0e
Lindabenrajab <128930943+Lindabenrajab@users.noreply.github.com>
2025-07-29 12:22:10 +0100

    Update app_with_web_crawler.py

--- app_with_web_crawler.py
+++ app_with_web_crawler.py
@@ -223,7 +223,10 @@ def main():
         st.session_state.faq_documents = faq_documents
 
         # Embeddings léger compatible CPU
-        embeddings = HuggingFaceEmbeddings(model_name="sentence-transformers/all-MiniLM-L6-v2")
+    embeddings = HuggingFaceEmbeddings(
+    model_name="sentence-transformers/all-MiniLM-L6-v2",
+    model_kwargs={"device": "cpu"}
+)
 
         vectorstore = FAISS.from_documents(faq_documents, embeddings)
         retriever = vectorstore.as_retriever(search_type="similarity", search_kwargs={"k": 8})
