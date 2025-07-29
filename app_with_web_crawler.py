d9b559eb001af54160973170508a3048ed3916da
Lindabenrajab <128930943+Lindabenrajab@users.noreply.github.com>
2025-07-29 11:58:00 +0100

    app_with_web_crawler.py

--- app_with_web_crawler.py
+++ app_with_web_crawler.py
@@ -3,7 +3,7 @@ import re
 import streamlit as st
 from dotenv import load_dotenv
 from langchain_core.documents import Document
-from langchain_community.vectorstores import FAISS
+from langchain.vectorstores import FAISS
 from langchain_huggingface import HuggingFaceEmbeddings
 from langchain_google_genai import ChatGoogleGenerativeAI
 from langchain_core.messages import HumanMessage, AIMessage, SystemMessage
